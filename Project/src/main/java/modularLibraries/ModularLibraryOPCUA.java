ff51a72afca0b977a762cf8fcebf567b30e52528
droferreira <dro.ferreira@campus.fct.unl.pt>
2024-06-12 14:27:40 +0100

    Update ModularLibraryOPCUA.java

--- Project/src/main/java/modularLibraries/ModularLibraryOPCUA.java
+++ Project/src/main/java/modularLibraries/ModularLibraryOPCUA.java
@@ -4,20 +4,19 @@ import io.netty.util.internal.StringUtil;
 import jade.util.Logger;
 import org.eclipse.milo.opcua.sdk.client.AddressSpace;
 import org.eclipse.milo.opcua.sdk.client.OpcUaClient;
-import org.eclipse.milo.opcua.sdk.client.api.config.OpcUaClientConfig;
 import org.eclipse.milo.opcua.sdk.client.api.config.OpcUaClientConfigBuilder;
-import org.eclipse.milo.opcua.sdk.client.api.identity.AnonymousProvider;
 import org.eclipse.milo.opcua.sdk.client.nodes.UaVariableNode;
 import org.eclipse.milo.opcua.sdk.client.subscriptions.ManagedDataItem;
 import org.eclipse.milo.opcua.sdk.client.subscriptions.ManagedSubscription;
 import org.eclipse.milo.opcua.stack.client.DiscoveryClient;
 import org.eclipse.milo.opcua.stack.core.AttributeId;
-import org.eclipse.milo.opcua.stack.core.types.builtin.*;
+import org.eclipse.milo.opcua.stack.core.types.builtin.DataValue;
+import org.eclipse.milo.opcua.stack.core.types.builtin.NodeId;
+import org.eclipse.milo.opcua.stack.core.types.builtin.StatusCode;
+import org.eclipse.milo.opcua.stack.core.types.builtin.Variant;
 import org.eclipse.milo.opcua.stack.core.types.structured.EndpointDescription;
-import org.eclipse.milo.opcua.stack.core.util.EndpointUtil;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
-import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
 import javax.xml.parsers.DocumentBuilder;
@@ -26,16 +25,13 @@ import java.io.File;
 import java.util.List;
 import java.util.logging.Level;
 
-import static org.eclipse.milo.opcua.stack.core.types.builtin.unsigned.Unsigned.uint;
-
 public class ModularLibraryOPCUA {
 
     String skill;
     String result;
     String endpointUrl;
-    String identifier;
-    NodeId nodeId;
-    int namespaceIndex;
+    NodeId reqNodeId;
+    NodeId resNodeId;
 
     volatile Boolean responseReceived = false;
     OpcUaClient client;
@@ -56,22 +52,24 @@ public class ModularLibraryOPCUA {
         Element configElement = xml.getDocumentElement();
         NodeList endpointUrlNode = configElement.getElementsByTagName("endpoint_url");
         NodeList namespaceIndexNode = configElement.getElementsByTagName("namespace_index");
-        NodeList identifierNode = configElement.getElementsByTagName("identifier");
+        NodeList requestsIdentifierNode = configElement.getElementsByTagName("requests_identifier");
+        NodeList responsesIdentifierNode = configElement.getElementsByTagName("responses_identifier");
 
         this.endpointUrl = endpointUrlNode.item(0).getTextContent();
-        this.namespaceIndex = Integer.parseInt(namespaceIndexNode.item(0).getTextContent());
-        this.identifier = identifierNode.item(0).getTextContent();
+        int namespaceIndex = Integer.parseInt(namespaceIndexNode.item(0).getTextContent());
+        String reqIdentifier = requestsIdentifierNode.item(0).getTextContent();
+        String resIdentifier = responsesIdentifierNode.item(0).getTextContent();
 
         try{
             List<EndpointDescription> endpointsList = DiscoveryClient.getEndpoints(endpointUrl).get();
-            EndpointDescription endpoint = EndpointUtil.updateUrl(endpointsList.get(0), "192.168.1.207", 54840);
 
             OpcUaClientConfigBuilder config = new OpcUaClientConfigBuilder();
-            config.setEndpoint(endpoint);
+            config.setEndpoint(endpointsList.getFirst());
 
             client = OpcUaClient.create(config.build());
             client.connect().get();
-            nodeId = new NodeId(namespaceIndex, identifier);
+            reqNodeId = new NodeId(namespaceIndex, reqIdentifier);
+            resNodeId = new NodeId(namespaceIndex, resIdentifier);
         } catch(Exception ex){
             Logger.getLogger(this.getClass().getName()).log(Level.SEVERE, null, ex);
         }
@@ -86,7 +84,7 @@ public class ModularLibraryOPCUA {
         try {
             AddressSpace addressSpace = client.getAddressSpace();
 
-            UaVariableNode node = (UaVariableNode) addressSpace.getNode(nodeId);
+            UaVariableNode node = (UaVariableNode) addressSpace.getNode(reqNodeId);
 
             while(true){
                 StatusCode statusCode = node.writeAttribute(
@@ -101,7 +99,7 @@ public class ModularLibraryOPCUA {
 
             ManagedSubscription subscription = ManagedSubscription.create(client);
 
-            ManagedDataItem dataItem = subscription.createDataItem(nodeId);
+            ManagedDataItem dataItem = subscription.createDataItem(resNodeId);
 
             if(!dataItem.getStatusCode().isGood()){
                 System.out.println("OPCUALib: Error in subscription");
@@ -110,14 +108,10 @@ public class ModularLibraryOPCUA {
             subscription.addChangeListener(new ManagedSubscription.ChangeListener() {
                 @Override
                 public void onDataReceived(List<ManagedDataItem> dataItems, List<DataValue> dataValues) {
-                    //System.out.println("AAAAAAAAAAAAAAAAA" + dataItems);
-                    //System.out.println("AAAAAAAAAAAAAAAAA" + dataValues);
-                    DataValue dataValue = dataValues.get(0);
+                    DataValue dataValue = dataValues.getFirst();
                     String res = ParseResult(dataValue);
-                    if(!res.equals(skill)){
-                        responseReceived = true;
-                        result = res;
-                    }
+                    responseReceived = true;
+                    result = res;
                 }
             });
 
