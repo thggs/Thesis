a8f0d12084bac9e4b6a61e938ac18fc09e476229
droferreira <dro.ferreira@campus.fct.unl.pt>
2024-06-12 22:38:49 +0100

    Update Main.java

--- Project/src/main/java/Main.java
+++ Project/src/main/java/Main.java
@@ -31,57 +31,58 @@ import java.util.concurrent.ExecutionException;
 
 public class Main {
 
-    static String endpointUrl = "opc.tcp://192.168.1.207:54840/Resources";
+    static String endpointUrl = "opc.tcp://192.168.1.207:53880";
     static String value;
 
     public static void main(String[] args) throws UaException, ExecutionException, InterruptedException, URISyntaxException, IOException {
+
+        OPCUA();
+
 //        List<EndpointDescription> endpoints = DiscoveryClient.getEndpoints(endpointUrl).get();
-//        EndpointDescription endpoint = EndpointUtil.updateUrl(endpoints.getFirst(), "192.168.1.207", 54840);
+//        //EndpointDescription endpoint = EndpointUtil.updateUrl(endpoints.getFirst(), "192.168.1.207", 53880);
 //
 //        for(EndpointDescription item : endpoints){
 //            System.out.println(item);
 //        }
 //
 //        OpcUaClientConfigBuilder configBuilder = new OpcUaClientConfigBuilder();
-//        configBuilder.setEndpoint(endpoint);
+//        configBuilder.setEndpoint(endpoints.getFirst());
 //
 //        OpcUaClient client = OpcUaClient.create(configBuilder.build());
 //
 //        client.connect().get();
 
-        try{
-            URL url = new URI("http://192.168.1.207:1880/conveyor").toURL();
-
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestMethod("POST");
-            connection.setRequestProperty("Test_key", "Test_value");
-
-            connection.setDoOutput(true);
-            OutputStream os = connection.getOutputStream();
-            os.write("payload=Source#TOKEN#C2".getBytes());
-            os.flush();
-            os.close();
-
-            int responseCode = connection.getResponseCode();
-            System.out.println("POST response Code: " + responseCode);
-
-            if(responseCode == HttpURLConnection.HTTP_OK){
-                BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-                String inputLine;
-                StringBuilder response = new StringBuilder();
-
-                while((inputLine = in.readLine()) != null){
-                    response.append(inputLine);
-                }
-                in.close();
-                System.out.println(response);
-            }
-        }
-        catch(Exception ex){
-            throw ex;
-        }
-
-
+//        try{
+//            URL url = new URI("http://192.168.1.207:1880/conveyor").toURL();
+//
+//            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+//            connection.setRequestMethod("POST");
+//            connection.setRequestProperty("Test_key", "Test_value");
+//
+//            connection.setDoOutput(true);
+//            OutputStream os = connection.getOutputStream();
+//            os.write("payload=Source#TOKEN#C2".getBytes());
+//            os.flush();
+//            os.close();
+//
+//            int responseCode = connection.getResponseCode();
+//            System.out.println("POST response Code: " + responseCode);
+//
+//            if(responseCode == HttpURLConnection.HTTP_OK){
+//                BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+//                String inputLine;
+//                StringBuilder response = new StringBuilder();
+//
+//                while((inputLine = in.readLine()) != null){
+//                    response.append(inputLine);
+//                }
+//                in.close();
+//                System.out.println(response);
+//            }
+//        }
+//        catch(Exception ex){
+//            throw ex;
+//        }
     }
 
     private static final Logger logger = LoggerFactory.getLogger(Main.class);
@@ -89,6 +90,7 @@ public class Main {
     private static void OPCUA() throws UaException, ExecutionException, InterruptedException {
 
         // Client connection START
+
 
         OpcUaClient client = OpcUaClient.create(
                 endpointUrl,
@@ -100,32 +102,17 @@ public class Main {
                     configBuilder.build()
         );
 
-//        endpoint = EndpointUtil.updateUrl(endpoint, "192.168.1.207");
-//
-//        OpcUaClientConfig config = OpcUaClientConfig.builder()
-//                .setApplicationName(LocalizedText.english("MultiAgentSystem"))
-//                .setApplicationUri("theURI")
-//                .setCertificate(null)
-//                .setKeyPair(null)
-//                .setEndpoint(endpoint)
-//                .setMaxResponseMessageSize(uint(50000))
-//                .setIdentityProvider(new AnonymousProvider())
-//                .setRequestTimeout(uint(5000))
-//                .build();
-//
-//        OpcUaClient client = OpcUaClient.create(config);
-
         client.connect().get();
 
         // Client connection END
 
         // Client write value START
 
-        NodeId nodeId = new NodeId(1, "Conveyor");
+        NodeId nodeIdReq = new NodeId(1, "Conveyor_Req");
 
         AddressSpace addressSpace = client.getAddressSpace();
 
-        UaVariableNode testNode = (UaVariableNode) addressSpace.getNode(nodeId);
+        UaVariableNode testNode = (UaVariableNode) addressSpace.getNode(nodeIdReq);
 
         StatusCode statusCode = testNode.writeAttribute(
                 AttributeId.Value,
@@ -136,9 +123,11 @@ public class Main {
 
         // Client subscription START
 
+        NodeId nodeIdRes = new NodeId(1, "Conveyor_Res");
+
         ManagedSubscription subscription = ManagedSubscription.create(client);
 
-        ManagedDataItem dataItem = subscription.createDataItem(nodeId);
+        ManagedDataItem dataItem = subscription.createDataItem(nodeIdRes);
 
         if(!dataItem.getStatusCode().isGood()){
             System.out.println("Error");
