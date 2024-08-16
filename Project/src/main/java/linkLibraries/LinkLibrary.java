9d95092ee99a47665f04ac7af878d6eee36754cf
droferreira <dro.ferreira@campus.fct.unl.pt>
2024-08-16 20:25:04 +0100

    Update LinkLibrary.java

--- Project/src/main/java/linkLibraries/LinkLibrary.java
+++ Project/src/main/java/linkLibraries/LinkLibrary.java
@@ -1,25 +1,8 @@
 package linkLibraries;
 
-import moduleEngine.ModuleEngine;
+public abstract class LinkLibrary {
 
-public class LinkLibrary {
+    public abstract String ExecuteSkill(String skill);
 
-    ModuleEngine moduleEngine;
-
-//    public LinkLibrary(){
-//        this.moduleEngine = new ModuleEngine();
-//    }
-//
-//    public Object createObject(String className, Object[] constructorParams){
-//        return moduleEngine.createObject(className, constructorParams);
-//    }
-//
-//    public Object executeMethod(Object obj, String methodName, Object[] methodParams){
-//        return moduleEngine.executeMethod(obj, methodName, methodParams);
-//    }
-
-    public void executeSkill(String skill){
-
-    }
-
+    public abstract void Stop();
 }
